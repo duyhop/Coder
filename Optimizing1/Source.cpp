769b96ccc688253859ec0b0068d8679057d7fd83
Bostney <duyhop3793@gmail.com>
2018-04-01 21:27:25 +0700

    Merge branch test

---
+++ Optimizing1/Source.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+//template <class T> void f(T &i) {cout << 1; }
+
+//template <class Type = int> void f(const int &i) {cout << 2; }
+
+class A {
+public:
+	int a;
+	
+	class B{
+	public:
+		int c;
+		void show()
+			{
+				cout << "test" << endl;
+			}
+		class C{
+		public:
+			int d;
+			
+		};
+	};
+	void A::B::show() {}
+};
+
+int main()
+{
+
+	return 0;
+}
